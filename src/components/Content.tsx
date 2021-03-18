8102b8e3242fa8e2fac0831b9017f4a461ad2e7f
gregory6z <gregoryrag@gmail.com>
2021-03-18 21:33:51 +0100

    testcompleted

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,39 @@
-export function Content() {
+import { MovieCard } from '../components/MovieCard';
+interface MovieProps {
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+interface ContentProps {
+  selectedGenre: GenreResponseProps;
+  movies: Array<MovieProps>;
+}
+export function Content({ movies, selectedGenre }: ContentProps) {
+
+  return (
   // Complete aqui
+  
+  <div className="container">
+    <header>
+      <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+    </header>
+
+    <main>
+      <div className="movies-list">
+        {movies.map(movie => (
+          <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+        ))}
+      </div>
+    </main>
+  </div>
+)
 }
